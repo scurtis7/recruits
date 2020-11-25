69e34496b09a20b1fc735029cd53610bfc8bba53
scurtis7 <scurtis7@gmail.com>
2020-11-25 18:53:46 -0500

    initial commit

---
+++ src/test/java/com/scurtis/recruits/RecruitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scurtis.recruits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
