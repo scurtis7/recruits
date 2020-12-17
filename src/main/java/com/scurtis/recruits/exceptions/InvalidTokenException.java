a3ad42408313d236eaa6832d9b30ec70f6efa776
scurtis7 <scurtis7@gmail.com>
2020-12-17 18:57:28 -0500

    cleanup

--- src/main/java/com/scurtis/recruits/exceptions/InvalidTokenException.java
+++ src/main/java/com/scurtis/recruits/exceptions/InvalidTokenException.java
@@ -9,8 +9,4 @@ public class InvalidTokenException extends RuntimeException {
     public InvalidTokenException(String message) {
         super(message);
     }
-
-    public InvalidTokenException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
