a9c2b2a8f6461979fc50e9daa69708e856d2c9d1
scurtis7 <scurtis7@gmail.com>
2020-12-17 20:36:35 -0500

    update session in ui

--- src/webapp/src/app/model/session.ts
+++ src/webapp/src/app/model/session.ts
@@ -1,9 +1,15 @@
 export class Session {
 
   id: string;
+  created: string;
+  expiration: number;
+  username: string;
 
-  constructor(id: string) {
+  constructor(id: string, created: string, expiration: number, username: string) {
     this.id = id;
+    this.created = created;
+    this.expiration = expiration;
+    this.username = username;
   }
 
 }
