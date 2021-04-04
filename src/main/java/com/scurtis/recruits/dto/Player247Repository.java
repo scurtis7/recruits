0115bbb6127d5e033f7cea73c5e252bd4f432371
Steve Curtis <scurtis7@gmail.com>
2021-04-04 14:33:06 -0400

    add data access and controller code for new player dropdowns

--- src/main/java/com/scurtis/recruits/dto/Player247Repository.java
+++ src/main/java/com/scurtis/recruits/dto/Player247Repository.java
@@ -1,6 +1,8 @@
 package com.scurtis.recruits.dto;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Author: Steve Curtis
@@ -8,4 +10,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
  **/
 
 public interface Player247Repository extends JpaRepository<Player247, Integer> {
+
+    List<Player247> findPlayer247ByCollege(String college);
+
+    List<Player247> findPlayer247ByCollegeAndYear(String college, Integer year);
+
+    List<Player247> findPlayer247ByCollegeAndPosition(String college, String position);
+
+    @Query("SELECT DISTINCT p.year FROM Player247 p WHERE p.college = ?1")
+    List<Integer> findDistinctYearsByCollege(String collge);
+
+    @Query("SELECT DISTINCT p.position FROM Player247 p WHERE p.college = ?1")
+    List<String> findDistinctPositionsByCollege(String college);
+
 }
--- src/test/java/com/scurtis/recruits/controller/PlayerControllerTest.java
+++ src/test/java/com/scurtis/recruits/controller/PlayerControllerTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,6 +19,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -47,12 +49,57 @@ class PlayerControllerTest {
     }
 
     @Test
-    void testGetAllPlayersSuccess() throws Exception {
-        mockMvc.perform(get("/api/players")
+    void testGetPlayersByCollegeSuccess() throws Exception {
+        Mockito.when(playerDataAccess.getAllPlayersByCollege("university")).thenReturn(getPlayers());
+        mockMvc.perform(get("/api/players/college/{college}", "university")
                 .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(getPlayers())))
-                .andExpect(status().isOk());
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(getPlayers())));
+    }
+
+    @Test
+    void testGetPlayersWithNoParameterReturns404NotFound() throws Exception {
+        mockMvc.perform(get("/api/players/college/")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testGetPlayersWithBadAcceptReturns406NotAcceptable() throws Exception {
+        mockMvc.perform(get("/api/players/college/{college}", "university")
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    void testGetPlayersByCollegeAndYearSuccess() throws Exception {
+        Mockito.when(playerDataAccess.getPlayersByCollegeAndYear("university", 2020)).thenReturn(getPlayers());
+        mockMvc.perform(get("/api/players/college/{college}/year/{year}", "university", "2020")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(getPlayers())));
+    }
+
+    @Test
+    void testGetPlayersByCollegeAndYearWithNoYearReturns404NotFound() throws Exception {
+        mockMvc.perform(get("/api/players/college/{university}/year/", "university")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testGetPlayersByCollegeAndYearWithBadAcceptReturns406NotAcceptable() throws Exception {
+        mockMvc.perform(get("/api/players/college/{college}/year/{year}", "university", 2020)
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotAcceptable());
     }
 
     private static List<Player247> getPlayers() {
