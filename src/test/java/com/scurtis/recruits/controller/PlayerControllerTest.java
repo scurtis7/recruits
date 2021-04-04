6ba53642346afd0ead1843fc03676ac49ef72466
Steve Curtis <scurtis7@gmail.com>
2021-04-04 14:58:34 -0400

    finish unit tests for new methods in player controller

--- src/test/java/com/scurtis/recruits/controller/PlayerControllerTest.java
+++ src/test/java/com/scurtis/recruits/controller/PlayerControllerTest.java
@@ -7,6 +7,8 @@ import com.scurtis.recruits.service.UserService;
 import com.scurtis.recruits.storage.PlayerDataAccess;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -42,21 +44,25 @@ class PlayerControllerTest {
     private PlayerDataAccess playerDataAccess;
 
     private static List<Player247> players;
+    private static List<Integer> years;
+    private static List<String> positions;
 
     @BeforeAll
     static void init() {
         players = getPlayers();
+        years = getYears();
+        positions = getPositions();
     }
 
     @Test
     void testGetPlayersByCollegeSuccess() throws Exception {
-        Mockito.when(playerDataAccess.getAllPlayersByCollege("university")).thenReturn(getPlayers());
+        Mockito.when(playerDataAccess.getAllPlayersByCollege("university")).thenReturn(players);
         mockMvc.perform(get("/api/players/college/{college}", "university")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectMapper.writeValueAsString(getPlayers())));
+                .andExpect(content().json(objectMapper.writeValueAsString(players)));
     }
 
     @Test
@@ -77,13 +83,13 @@ class PlayerControllerTest {
 
     @Test
     void testGetPlayersByCollegeAndYearSuccess() throws Exception {
-        Mockito.when(playerDataAccess.getPlayersByCollegeAndYear("university", 2020)).thenReturn(getPlayers());
+        Mockito.when(playerDataAccess.getPlayersByCollegeAndYear("university", 2020)).thenReturn(players);
         mockMvc.perform(get("/api/players/college/{college}/year/{year}", "university", "2020")
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectMapper.writeValueAsString(getPlayers())));
+                .andExpect(content().json(objectMapper.writeValueAsString(players)));
     }
 
     @Test
@@ -97,6 +103,87 @@ class PlayerControllerTest {
     @Test
     void testGetPlayersByCollegeAndYearWithBadAcceptReturns406NotAcceptable() throws Exception {
         mockMvc.perform(get("/api/players/college/{college}/year/{year}", "university", 2020)
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    void testGetPlayersByCollegeAndPositionSuccess() throws Exception {
+        Mockito.when(playerDataAccess.getPlayersByCollegeAndPosition("university", "position")).thenReturn(players);
+        mockMvc.perform(get("/api/players/college/{college}/position/{position}", "university", "position")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(players)));
+    }
+
+    @Test
+    void testGetPlayersByCollegeAndPositionWithNoYearReturns404NotFound() throws Exception {
+        mockMvc.perform(get("/api/players/college/{university}/position/", "university")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testGetPlayersByCollegeAndPositionWithBadAcceptReturns406NotAcceptable() throws Exception {
+        mockMvc.perform(get("/api/players/college/{college}/position/{position}", "university", "position")
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    void testGetDistinctYearsByCollegeSuccess() throws Exception {
+        Mockito.when(playerDataAccess.getDistinctYearsByCollege("university")).thenReturn(years);
+        mockMvc.perform(get("/api/distinct/years/college/{college}/", "university")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(years)));
+    }
+
+    @Test
+    void testGetDistinctYearsByCollegeWithNoCollegeReturns404NotFound() throws Exception {
+        mockMvc.perform(get("/api/distinct/years/college/")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testGetDistinctYearsByCollegeWithBadAcceptReturns406NotAcceptable() throws Exception {
+        mockMvc.perform(get("/api/distinct/years/college/{college}/", "university")
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    void testGetDistinctPositionsByCollegeSuccess() throws Exception {
+        Mockito.when(playerDataAccess.getDistinctPositionsByCollege("university")).thenReturn(positions);
+        mockMvc.perform(get("/api/distinct/positions/college/{college}/", "university")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(positions)));
+    }
+
+    @Test
+    void testGetDistinctPositionsByCollegeWithNoCollegeReturns404NotFound() throws Exception {
+        mockMvc.perform(get("/api/distinct/positions/college/")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testGetDistinctPositionsByCollegeWithBadAcceptReturns406NotAcceptable() throws Exception {
+        mockMvc.perform(get("/api/distinct/positions/college/{college}/", "university")
                 .accept(MediaType.TEXT_PLAIN)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNotAcceptable());
@@ -122,6 +209,16 @@ class PlayerControllerTest {
         player.setLink("link");
         player.setCollege("college");
         return Collections.singletonList(player);
+    }
+
+    private static List<Integer> getYears() {
+        return Stream.of(2018, 2019, 2020, 2021, 2022)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> getPositions() {
+        return Stream.of("QB", "CB", "Running Back")
+                .collect(Collectors.toList());
     }
 
 }
